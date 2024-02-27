e89deaf64d83e3e1d82f0490da696929770a8565
GopiMunipalle <munipallegopikrishna@gmail.com>
2024-02-27 18:24:40 +0530

    updated

--- test/integration/auth.test.ts
+++ test/integration/auth.test.ts
@@ -47,7 +47,7 @@ import bcrypt from 'bcrypt';
 //       expect(response.body).toEqual({ error: 'User Exists Already' });
 //     });
 
-//     // Add more test cases for invalid input, edge cases, etc.
+    
 //   });
 
 //   describe('login API', () => {
@@ -91,44 +91,86 @@ import bcrypt from 'bcrypt';
 //   });
 // });
 
-describe('User controller',()=>{
+describe('User Controller',()=>{
+  const mockUser={email:"gopi@gmail.com"}
   beforeAll(async()=>{
-    const hashedPassword=await bcrypt.hash('gopi',10)
-    await userModel.create({
-      name:'gopi',
-      email:'gopi1@gmail.com',
-      password:hashedPassword,
-      number:'9988776655'
+    const hashedPassword=await bcrypt.hash('testPassword',10)
+    const user=userModel.create({
+      name:'name',
+      email:'test@gmail.com',
+      password:'Password33',
+      number:'333333332'
     })
   })
-
   afterAll(async()=>{
     await userModel.deleteMany({})
-  })  
+  })
 
-  describe('signUp Route',()=>{
+  describe('SignUp User model',()=>{
     it('signUp',async()=>{
       const response=await request(app)
       .post('/user/signUp')
       .send({
-        name:'gopi',
-        email:"gopi2@gmail.com",
-        password:'Gopi@123',
-        number:'9988776655'
+        name:"gopi",
+        email:"gopi@gmail.com",
+        password:"Gopi@123",
+        number:"9988776655"
       })
-       console.log("hello")
-      expect(response.status).toBe(200)
+      expect(response.status).toBe(201)
       expect(response.body).toEqual({message:"User Created Successfully"})
     })
-    it('user exists',async()=>{
+    it('User Exists',async()=>{
       const response=await request(app)
       .post('/user/signUp')
       .send({
-        email:'gopi3@gmail.com'
+        name:"gopi",
+        email:'gopi@gmail.com',
+        password:"Gopi@123",
+        number:"9988776655"
       })
       expect(response.status).toBe(400)
       expect(response.body).toEqual({error:"User Exists Already"})
     })
   })
-  
+
+  describe('Login User',()=>{
+    it('Login User',async()=>{
+      const response=await request(app)
+      .post('/user/login')
+      .send({
+        email:"gopi@gmail.com",
+        password:"Gopi@123"
+      })
+      expect(response.status).toBe(200)
+      expect(response.body).toHaveProperty('jwtToken')
+    })
+    it('Incorrect Email',async()=>{
+      const response=await request(app)
+      .post('/user/login')
+      .send({
+        email:"gop@gmail.com",
+        password:"Gopi@123"
+      })
+      expect(response.status).toBe(404)
+      expect(response.body).toEqual({error:"User not found"})
+    })
+    it('Incorrect Password',async()=>{
+      const response=await request(app)
+      .post('/user/login')
+      .send({
+        email:'gopi@gmail.com',
+        password:"gopi"
+      })
+      expect(response.status).toBe(400)
+      expect(response.body).toEqual({error:"Incorrect Password"})
+    })
+  })
+
+  describe('GetLogin User',()=>{
+    it('Get Login',async()=>{
+      const respose=await request(app)
+      .get('/user/getLoginUser')
+    })
+  })
 })
+
