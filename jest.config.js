638db11a8762490c689b5e7fda411fbc56211bce
GopiMunipalle <munipallegopikrishna@gmail.com>
2024-02-28 10:55:25 +0530

    commit

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  baseUrl: "http://localhost:8080",
-  authorization: { "Authorization":"authvalue" }
-};
